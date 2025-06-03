069a19049149b90ba3e31a6f79b0c3ebfdd5c537
Arjun Singh Rawat <asrawat@geduservices.com>
2025-06-04 01:00:32 +0530

    file updated

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   optimizeDeps: {
-    include: ["rtl-react-hooks"],
-  }
+  optimizeDeps: {
+    include: ['rtl-react-hooks'],
+  },
+  build: {
+    outDir: 'build',
+  },
 })

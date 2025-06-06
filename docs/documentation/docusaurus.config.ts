2ff7576ae648b5ae290f07364dbf0ef74a7dfb8b
Arjun Singh Rawat <asrawat@geduservices.com>
2025-06-06 18:02:18 +0530

    code updated

--- docs/documentation/docusaurus.config.ts
+++ docs/documentation/docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Rexora', // Usually your GitHub org/user name.
+  projectName: 'documentation ', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

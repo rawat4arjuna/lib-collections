c389ce382654953207055a69f56dc772c493dfa1
Arjun Singh Rawat <asrawat@geduservices.com>
2025-06-06 16:08:07 +0530

    github url removed

--- docs/rtl-hooks-docs/docusaurus.config.ts
+++ docs/rtl-hooks-docs/docusaurus.config.ts
@@ -86,11 +86,11 @@ const config: Config = {
           label: 'Tutorial',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
